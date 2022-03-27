00acde55513000687734f5a3da2dfdda80ad4568
Ben <benjamin.mahmoudi@gmail.com>
2022-03-27 11:07:52 +0200

    core: add customSetInterval helper with test

--- src/Helpers/helpers.test.ts
+++ src/Helpers/helpers.test.ts
@@ -1,4 +1,4 @@
-import { randomNumberBetween } from ".";
+import { customSetInterval, randomNumberBetween } from ".";
 
 describe("randomNumberBetween helper", () => {
 	it("should always return a random number between 0.5 and 2", () => {
@@ -6,5 +6,15 @@ describe("randomNumberBetween helper", () => {
 			expect(randomNumberBetween({ maxValue: 2, minValue: 0.5 })).toBeGreaterThanOrEqual(0.5);
 			expect(randomNumberBetween({ maxValue: 2, minValue: 0.5 })).toBeLessThan(2);
 		}
+	});
+});
+
+describe("customSetInterval helper", () => {
+	jest.useFakeTimers();
+	const callback = jest.fn();
+	it("should call the callback 10 times in 10 seconds", () => {
+		customSetInterval({ callback, setIntervalDuration: () => 1000 });
+		jest.advanceTimersByTime(10000);
+		expect(callback).toHaveBeenCalledTimes(10);
 	});
 });
--- src/Helpers/index.tsx
+++ src/Helpers/index.tsx
@@ -5,3 +5,23 @@
 export const randomNumberBetween = (
 	{ minValue, maxValue }: { minValue: number; maxValue: number },
 ): number => Math.random() * (maxValue - minValue) + minValue;
+
+/**
+ * A recursive function that replicates "setInterval", with the ability to set a different interval for every callback triggered
+ */
+export const customSetInterval = (
+	{ callback, getTimeoutIdentifier, setIntervalDuration = () => 0 }: {
+		callback: () => void;
+		getTimeoutIdentifier?: (identifier: NodeJS.Timeout) => void;
+		setIntervalDuration?: () => number,
+	},
+): void => {
+	const timeoutIdentifier = setTimeout(
+		() => {
+			callback();
+			customSetInterval({ callback, getTimeoutIdentifier, setIntervalDuration });
+		},
+		setIntervalDuration(),
+	);
+	getTimeoutIdentifier?.(timeoutIdentifier);
+};
